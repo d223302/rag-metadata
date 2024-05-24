fe0640ea91a9291b548f2248df6008d80ab62ac9
David <d223302@gmail.com>
2024-05-24 19:32:15 +0800

    Add src

--- run.sh
+++ run.sh
@@ -36,48 +36,47 @@
 #  done
 
 
-for model in "meta-llama/Meta-Llama-3-8B-Instruct"; do
-    for prompt_template in "input_rank_no_google"; do
-        for modify_meta_data in 1 0; do
-            if [[ $modify_meta_data == 1 ]]; then
-                for favored_stance in "yes" "no"; do
-                    python3 main.py \
-                        --model_name "$model" \
-                        --prompt_template "$prompt_template" \
-                        --favored_stance "$favored_stance" \
-                        --modify_meta_data "$modify_meta_data" \
-                        --dataset_path data/fake_knowledge_with_evidence_parsed.json \
-                        --output_dir results_fake
-
-                done
-            else
-                python3 main.py \
-                    --model_name "$model" \
-                    --prompt_template "$prompt_template" \
-                    --modify_meta_data "$modify_meta_data" \
-                    --dataset_path data/fake_knowledge_with_evidence_parsed.json \
-                    --output_dir results_fake
-            fi
-        done
-    done
-done
-
-#   for model in "meta-llama/Meta-Llama-3-8B-Instruct"; do
-#       for url_modifier in "wiki_wordpress_url" "cnn_naturalnews_url"; do
-#         for prompt_template in "input_url" "input_emphasize_url"; do
-#           for modify_meta_data in 1; do
-#              for favored_stance in "yes" "no"; do
+#  for model in "meta-llama/Meta-Llama-3-8B-Instruct" "meta-llama/Llama-2-7b-chat-hf" "meta-llama/Llama-2-13b-chat-hf" "meta-llama/Llama-2-70b-chat-hf" "allenai/tulu-2-dpo-7b" "allenai/tulu-2-dpo-70b" "meta-llama/Meta-Llama-3-70B-Instruct"; do
+#      for prompt_template in "input_rank_no_google"; do
+#          for modify_meta_data in 1; do
+#              if [[ $modify_meta_data == 1 ]]; then
+#                  for favored_stance in "yes" "no"; do
+#                      python3 main.py \
+#                          --model_name "$model" \
+#                          --prompt_template "$prompt_template" \
+#                          --favored_stance "$favored_stance" \
+#                          --modify_meta_data "$modify_meta_data" \
+#                          --dataset_path data/fake_knowledge_with_evidence_parsed.json \
+#                          --output_dir results_fake
+#  
+#                  done
+#              else
 #                  python3 main.py \
 #                      --model_name "$model" \
 #                      --prompt_template "$prompt_template" \
-#                      --url_modifier "$url_modifier" \
-#                      --favored_stance "$favored_stance" \
 #                      --modify_meta_data "$modify_meta_data" \
 #                      --dataset_path data/fake_knowledge_with_evidence_parsed.json \
 #                      --output_dir results_fake
-#               done
-#           done
-#         done
-#       done
-#   done
-#   
+#              fi
+#          done
+#      done
+#  done
+
+for model in "meta-llama/Meta-Llama-3-8B-Instruct" "meta-llama/Llama-2-7b-chat-hf" "meta-llama/Llama-2-13b-chat-hf" "meta-llama/Llama-2-70b-chat-hf" "allenai/tulu-2-dpo-7b" "allenai/tulu-2-dpo-70b" "meta-llama/Meta-Llama-3-70B-Instruct"; do
+    for url_modifier in "wiki_wordpress_src" "cnn_naturalnews_src"; do
+      for prompt_template in "input_emphasize_src"; do
+        for modify_meta_data in 1; do
+           for favored_stance in "yes" "no"; do
+               python3 main.py \
+                   --model_name "$model" \
+                   --prompt_template "$prompt_template" \
+                   --url_modifier "$url_modifier" \
+                   --favored_stance "$favored_stance" \
+                   --modify_meta_data "$modify_meta_data" \
+                   --dataset_path data/fake_knowledge_with_evidence_parsed.json \
+                   --output_dir results_fake
+            done
+        done
+      done
+    done
+done
