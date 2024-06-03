ebf047f0587b87d6f9b9bd71e5015fded5321f8a
d223302 <d223302@gmail.com>
2024-06-03 14:05:27 +0800

    Correct wrong hard-code

--- run_generate.sh
+++ run_generate.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-# source ~/.bashrc
-# conda activate unsloth_env
+source ~/.bashrc
+conda activate unsloth_env
 #  for model in "allenai/tulu-2-dpo-7b"; do
 #      for prompt_template in "input_date" "input_date_today" "input_url" "input_rank" "input_emphasize_url" "input_emphasize_wiki_url"; do
 #          for modify_meta_data in 0 1; do
@@ -27,39 +27,39 @@
 #  
 
 
-# for model in "meta-llama/Llama-2-13b-chat-hf" "meta-llama/Llama-2-7b-chat-hf"; do
-#     for prompt_template in "input_rank_no_google"; do
-#         for modify_meta_data in 1; do
-#             if [[ $modify_meta_data == 1 ]]; then
-#                 for favored_stance in "yes" "no"; do
-#                     python3 text_llm.py \
-#                         --generation \
-#                         --model_name "$model" \
-#                         --prompt_template "$prompt_template" \
-#                         --favored_stance "$favored_stance" \
-#                         --modify_meta_data "$modify_meta_data" \
-#                         --dataset_path data/fake_knowledge_with_evidence_parsed.json \
-#                         --output_dir results_fake
-# 
-#                 done
-#             else
-#                 python3 text_llm.py \
-#                     --generation \
-#                     --model_name "$model" \
-#                     --prompt_template "$prompt_template" \
-#                     --modify_meta_data "$modify_meta_data" \
-#                     --dataset_path data/fake_knowledge_with_evidence_parsed.json \
-#                     --output_dir results_fake
-#             fi
-#         done
-#     done
-# done
+#  for model in "meta-llama/Llama-2-13b-chat-hf" "meta-llama/Llama-2-7b-chat-hf"; do
+#      for prompt_template in "input_rank_no_google"; do
+#          for modify_meta_data in 1; do
+#              if [[ $modify_meta_data == 1 ]]; then
+#                  for favored_stance in "yes" "no"; do
+#                      python3 text_llm.py \
+#                          --generation \
+#                          --model_name "$model" \
+#                          --prompt_template "$prompt_template" \
+#                          --favored_stance "$favored_stance" \
+#                          --modify_meta_data "$modify_meta_data" \
+#                          --dataset_path data/fake_knowledge_with_evidence_parsed.json \
+#                          --output_dir results_fake
+#  
+#                  done
+#              else
+#                  python3 text_llm.py \
+#                      --generation \
+#                      --model_name "$model" \
+#                      --prompt_template "$prompt_template" \
+#                      --modify_meta_data "$modify_meta_data" \
+#                      --dataset_path data/fake_knowledge_with_evidence_parsed.json \
+#                      --output_dir results_fake
+#              fi
+#          done
+#      done
+#  done
 
 
 
-for model in "allenai/tulu-2-dpo-7b"; do
-    for url_modifier in 'cnn_naturalnews_url' 'wiki_wordpress_url'; do
-      for prompt_template in "input_emphasize_url"; do
+for model in "meta-llama/Meta-Llama-3-8B-Instruct"; do
+    for url_modifier in 'cnn_naturalnews_src' 'wiki_wordpress_src'; do
+      for prompt_template in "input_emphasize_src"; do
         for modify_meta_data in 1; do
             if [[ $modify_meta_data == 1 ]]; then
                 for favored_stance in "yes" "no"; do
