08046e1013f301cec431f5367e70fb845a9bb8ca
David <d223302@gmail.com>
2024-06-04 13:41:44 +0800

    Add parser

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 #      for prompt_template in "input_no_meta" "input_date" "input_url" "input_rank" "input_emphasize_url"; do
 #          for modify_meta_data in 0 1; do
 #              for favored_stance in "yes" "no"; do
-#                  python3 main.py \
+#                  python3 text_llm.py \
 #                      --model_$name model \
 #                      --prompt_template $prompt_template \
 #                      --favored_stance $favored_stance \
@@ -14,34 +14,35 @@
 #  done
 # input_no_meta" "input_date" "input_date_today" "input_url" "input_rank" "input_emphasize_url" "input_emphasize_wiki_url"
 
+# for model in "meta-llama/Meta-Llama-3-70B-Instruct"; do
+#     for prompt_template in "input_no_meta"; do
+#         for modify_meta_data in 0 ; do
+#             if [[ $modify_meta_data == 1 ]]; then
+#                 for favored_stance in "yes" "no"; do
+#                     python3 text_llm.py \
+#                         --model_name "$model" \
+#                         --prompt_template "$prompt_template" \
+#                         --favored_stance "$favored_stance" \
+#                         --modify_meta_data "$modify_meta_data"
+#                 done
+#             else
+#                 python3 text_llm.py \
+#                     --model_name "$model" \
+#                     --prompt_template "$prompt_template" \
+#                     --modify_meta_data "$modify_meta_data"
+#             fi
+#         done
+#     done
+# done
+
+# "meta-llama/Meta-Llama-3-8B-Instruct" "meta-llama/Llama-2-7b-chat-hf" "meta-llama/Llama-2-13b-chat-hf" "meta-llama/Llama-2-70b-chat-hf" "allenai/tulu-2-dpo-7b" "allenai/tulu-2-dpo-70b" 
+
 #  for model in "meta-llama/Meta-Llama-3-70B-Instruct"; do
-#      for prompt_template in "input_no_meta"; do
-#          for modify_meta_data in 0 ; do
-#              if [[ $modify_meta_data == 1 ]]; then
-#                  for favored_stance in "yes" "no"; do
-#                      python3 main.py \
-#                          --model_name "$model" \
-#                          --prompt_template "$prompt_template" \
-#                          --favored_stance "$favored_stance" \
-#                          --modify_meta_data "$modify_meta_data"
-#                  done
-#              else
-#                  python3 main.py \
-#                      --model_name "$model" \
-#                      --prompt_template "$prompt_template" \
-#                      --modify_meta_data "$modify_meta_data"
-#              fi
-#          done
-#      done
-#  done
-
-
-#  for model in "meta-llama/Meta-Llama-3-8B-Instruct" "meta-llama/Llama-2-7b-chat-hf" "meta-llama/Llama-2-13b-chat-hf" "meta-llama/Llama-2-70b-chat-hf" "allenai/tulu-2-dpo-7b" "allenai/tulu-2-dpo-70b" "meta-llama/Meta-Llama-3-70B-Instruct"; do
-#      for prompt_template in "input_rank_no_google"; do
+#      for prompt_template in "input_no_meta" "input_date" "input_date_today" "input_url" "input_rank" "input_emphasize_url" "input_emphasize_wiki_url"; do
 #          for modify_meta_data in 1; do
 #              if [[ $modify_meta_data == 1 ]]; then
 #                  for favored_stance in "yes" "no"; do
-#                      python3 main.py \
+#                      python3 text_llm.py \
 #                          --model_name "$model" \
 #                          --prompt_template "$prompt_template" \
 #                          --favored_stance "$favored_stance" \
@@ -51,7 +52,7 @@
 #  
 #                  done
 #              else
-#                  python3 main.py \
+#                  python3 text_llm.py \
 #                      --model_name "$model" \
 #                      --prompt_template "$prompt_template" \
 #                      --modify_meta_data "$modify_meta_data" \
@@ -62,12 +63,12 @@
 #      done
 #  done
 
-for model in "meta-llama/Meta-Llama-3-8B-Instruct" "meta-llama/Llama-2-7b-chat-hf" "meta-llama/Llama-2-13b-chat-hf" "allenai/tulu-2-dpo-7b"; do
+for model in "meta-llama/Meta-Llama-3-70B-Instruct"; do
     for url_modifier in "wiki_wordpress_src" "cnn_naturalnews_src"; do
       for prompt_template in "input_emphasize_src"; do
         for modify_meta_data in 1; do
            for favored_stance in "yes" "no"; do
-               python3 main.py \
+               python3 text_llm.py \
                    --model_name "$model" \
                    --prompt_template "$prompt_template" \
                    --url_modifier "$url_modifier" \
