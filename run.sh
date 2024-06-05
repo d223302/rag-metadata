6a965dc5d673fa6541cd57e9d68ca0a6e712fb1e
d223302 <d223302@gmail.com>
2024-06-05 13:35:14 +0800

    Modify generation

--- run.sh
+++ run.sh
@@ -1,18 +1,40 @@
 #!/usr/bin/env bash
-#  for model in "meta-llama/Meta-Llama-3-8B-Instruct" "meta-llama/Llama-2-7b-chat-hf" "meta-llama/Llama-2-13b-chat-hf" "meta-llama/Llama-2-70b-chat-hf" "allenai/tulu-2-dpo-7b" "allenai/tulu-2-dpo-70b"; do
-#      for prompt_template in "input_no_meta" "input_date" "input_url" "input_rank" "input_emphasize_url"; do
-#          for modify_meta_data in 0 1; do
-#              for favored_stance in "yes" "no"; do
-#                  python3 text_llm.py \
-#                      --model_$name model \
-#                      --prompt_template $prompt_template \
-#                      --favored_stance $favored_stance \
-#                      --modify_meta_data $modify_meta_data
-#              done
-#          done
-#      done
-#  done
+
+# for model in "meta-llama/Meta-Llama-3-8B-Instruct" "meta-llama/Llama-2-7b-chat-hf" "meta-llama/Llama-2-13b-chat-hf" "meta-llama/Llama-2-70b-chat-hf" "allenai/tulu-2-dpo-7b" "allenai/tulu-2-dpo-70b"; do
+#     for prompt_template in "input_no_meta" "input_date" "input_url" "input_rank" "input_emphasize_url"; do
+#         for modify_meta_data in 0 1; do
+#             for favored_stance in "yes" "no"; do
+#                 python3 text_llm.py \
+#                     --model_$name model \
+#                     --prompt_template $prompt_template \
+#                     --favored_stance $favored_stance \
+#                     --modify_meta_data $modify_meta_data
+#             done
+#         done
+#     done
+# done
+
 # input_no_meta" "input_date" "input_date_today" "input_url" "input_rank" "input_emphasize_url" "input_emphasize_wiki_url"
+
+
+for model in "gpt-4-turbo-2024-04-09"; do
+    for url_modifier in "wiki_wordpress_src" "cnn_naturalnews_src"; do
+      for prompt_template in "input_emphasize_src"; do
+        for modify_meta_data in 1; do
+           for favored_stance in "yes" "no"; do
+                python3 text_llm.py \
+                   --model_name "$model" \
+                   --prompt_template "$prompt_template" \
+                   --url_modifier "$url_modifier" \
+                   --favored_stance "$favored_stance" \
+                   --modify_meta_data "$modify_meta_data" \
+                   --dataset_path data/fake_knowledge_with_evidence_parsed.json \
+                   --output_dir results_fake
+            done
+        done
+      done
+    done
+done
 
 # for model in "meta-llama/Meta-Llama-3-70B-Instruct"; do
 #     for prompt_template in "input_no_meta"; do
@@ -83,21 +105,22 @@
 # done
 # 
 # 
-for model in "gpt-4-turbo-2024-04-09"; do
-    for url_modifier in "wiki_wordpress_src" "cnn_naturalnews_src"; do
-      for prompt_template in "input_emphasize_src"; do
-        for modify_meta_data in 1; do
-           for favored_stance in "yes" "no"; do
-                python3 text_llm.py \
-                   --model_name "$model" \
-                   --prompt_template "$prompt_template" \
-                   --url_modifier "$url_modifier" \
-                   --favored_stance "$favored_stance" \
-                   --modify_meta_data "$modify_meta_data" \
-                   --dataset_path data/fake_knowledge_with_evidence_parsed.json \
-                   --output_dir results_fake
-            done
-        done
-      done
-    done
-done
+
+#  for model in "gpt-4-turbo-2024-04-09"; do
+#      for url_modifier in "wiki_wordpress_src" "cnn_naturalnews_src"; do
+#        for prompt_template in "input_emphasize_src"; do
+#          for modify_meta_data in 1; do
+#             for favored_stance in "yes" "no"; do
+#                  python3 text_llm.py \
+#                     --model_name "$model" \
+#                     --prompt_template "$prompt_template" \
+#                     --url_modifier "$url_modifier" \
+#                     --favored_stance "$favored_stance" \
+#                     --modify_meta_data "$modify_meta_data" \
+#                     --dataset_path data/fake_knowledge_with_evidence_parsed.json \
+#                     --output_dir results_fake
+#              done
+#          done
+#        done
+#      done
+#  done
