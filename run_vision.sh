3b8a6f9d9f116f716c79cef5b8d209457dd54d49
David <d223302@gmail.com>
2024-06-12 13:03:44 +0800

    Add openai generation

--- run_vision.sh
+++ run_vision.sh
@@ -18,39 +18,16 @@
 #  done
 
 
-# for model in claude-3-sonnet-20240229; do
-#     for yes_html_template in "pretty" "simple"; do
-#         for no_html_template in "pretty" "simple"; do
-#             for prompt_template in "vision_prompts"; do
-#                 if [ "$yes_html_template" == "$no_html_template" ]; then
-#                     continue
-#                 fi
-#                 python3 vision_llm.py \
-#                     --model_name "$model" \
-#                     --generation \
-#                     --max_tokens 512 \
-#                     --prompt_template "$prompt_template" \
-#                     --dataset_path data/fake_knowledge_with_evidence_parsed.json \
-#                     --yes_html_template "$yes_html_template" \
-#                     --no_html_template "$no_html_template" \
-#                     --image_dir data/imgs/fake \
-#                     --output_dir results_vision_fake
-#             done
-#         done
-#     done
-# done
-
-for model in "claude-3-sonnet-20240229"; do
+for model in "gpt-4o"; do
     for yes_html_template in "pretty" "simple"; do
         for no_html_template in "pretty" "simple"; do
-            for prompt_template in "vision_prompts"; do
+            for prompt_template in "vision_prompts_with_text"; do
                 if [ "$yes_html_template" == "$no_html_template" ]; then
                     continue
                 fi
                 python3 vision_llm.py \
                     --model_name "$model" \
                     --generation \
-                    --credibility \
                     --max_tokens 512 \
                     --prompt_template "$prompt_template" \
                     --dataset_path data/fake_knowledge_with_evidence_parsed.json \
@@ -62,3 +39,26 @@ for model in "claude-3-sonnet-20240229"; do
         done
     done
 done
+
+# for model in "claude-3-sonnet-20240229"; do
+#     for yes_html_template in "pretty" "simple"; do
+#         for no_html_template in "pretty" "simple"; do
+#             for prompt_template in "vision_prompts"; do
+#                 if [ "$yes_html_template" == "$no_html_template" ]; then
+#                     continue
+#                 fi
+#                 python3 vision_llm.py \
+#                     --model_name "$model" \
+#                     --generation \
+#                     --credibility \
+#                     --max_tokens 512 \
+#                     --prompt_template "$prompt_template" \
+#                     --dataset_path data/fake_knowledge_with_evidence_parsed.json \
+#                     --yes_html_template "$yes_html_template" \
+#                     --no_html_template "$no_html_template" \
+#                     --image_dir data/imgs/fake \
+#                     --output_dir results_vision_fake
+#             done
+#         done
+#     done
+# done
