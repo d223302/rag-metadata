d2c21e129c1a3f9c79bb57a5d692625e8ef73993
David <d223302@gmail.com>
2024-06-16 09:40:00 +0800

    add something

--- run.sh
+++ run.sh
@@ -16,35 +16,35 @@
 
 # input_no_meta" "input_date" "input_date_today" "input_url" "input_rank" "input_emphasize_url" "input_emphasize_wiki_url"
 
-for model in "gpt-4-turbo-2024-04-09"; do
-    for url_modifier in "wiki_wordpress_src"; do
-      for prompt_template in "input_emphasize_src"; do
-        for modify_meta_data in 1; do
-            if [[ $modify_meta_data == 1 ]]; then
-                for favored_stance in "yes" "no"; do
-                    python3 text_llm.py \
-                        --dataset_path data_with_keyword.json \
-                        --url_modifier "$url_modifier" \
-                        --model_name "$model" \
-                        --prompt_template "$prompt_template" \
-                        --favored_stance "$favored_stance" \
-                        --modify_meta_data "$modify_meta_data"
+#  for model in "gpt-4-turbo-2024-04-09"; do
+#      for url_modifier in "wiki_wordpress_src"; do
+#        for prompt_template in "input_emphasize_src"; do
+#          for modify_meta_data in 1; do
+#              if [[ $modify_meta_data == 1 ]]; then
+#                  for favored_stance in "yes" "no"; do
+#                      python3 text_llm.py \
+#                          --dataset_path data_with_keyword.json \
+#                          --url_modifier "$url_modifier" \
+#                          --model_name "$model" \
+#                          --prompt_template "$prompt_template" \
+#                          --favored_stance "$favored_stance" \
+#                          --modify_meta_data "$modify_meta_data"
+#  
+#                  done
+#              else
+#                  python3 text_llm.py \
+#                      --dataset_path data_with_keyword.json \
+#                      --url_modifier "$url_modifier" \
+#                      --model_name "$model" \
+#                      --prompt_template "$prompt_template" \
+#                      --modify_meta_data "$modify_meta_data"
+#              fi
+#          done
+#        done
+#      done
+#  done
 
-                done
-            else
-                python3 text_llm.py \
-                    --dataset_path data_with_keyword.json \
-                    --url_modifier "$url_modifier" \
-                    --model_name "$model" \
-                    --prompt_template "$prompt_template" \
-                    --modify_meta_data "$modify_meta_data"
-            fi
-        done
-      done
-    done
-done
-
-for model in "gpt-4-turbo-2024-04-09"; do
+for model in "meta-llama/Meta-Llama-3-70B-Instruct"; do
     for url_modifier in "wiki_wordpress_url"; do
       for prompt_template in "input_emphasize_url"; do
         for modify_meta_data in 1; do
@@ -122,24 +122,24 @@ done
 #     done
 # done
 # 
-for model in "claude-3-haiku-20240307"; do
-    for url_modifier in "wiki_wordpress_url" "cnn_naturalnews_url"; do
-      for prompt_template in "input_emphasize_url"; do
-        for modify_meta_data in 1; do
-           for favored_stance in "yes" "no"; do
-               python3 text_llm.py \
-                   --model_name "$model" \
-                   --prompt_template "$prompt_template" \
-                   --url_modifier "$url_modifier" \
-                   --favored_stance "$favored_stance" \
-                   --modify_meta_data "$modify_meta_data" \
-                   --dataset_path data/fake_knowledge_with_evidence_parsed.json \
-                   --output_dir results_fake
-            done
-        done
-      done
-    done
-done
+# for model in "meta-llama/Meta-Llama-3-70B-Instruct"; do
+#     for url_modifier in "wiki_wordpress_url" "cnn_naturalnews_url"; do
+#       for prompt_template in "input_emphasize_url"; do
+#         for modify_meta_data in 1; do
+#            for favored_stance in "yes" "no"; do
+#                python3 text_llm.py \
+#                    --model_name "$model" \
+#                    --prompt_template "$prompt_template" \
+#                    --url_modifier "$url_modifier" \
+#                    --favored_stance "$favored_stance" \
+#                    --modify_meta_data "$modify_meta_data" \
+#                    --dataset_path data/fake_knowledge_with_evidence_parsed.json \
+#                    --output_dir results_fake
+#             done
+#         done
+#       done
+#     done
+# done
 #  
 #  
 #  for model in "claude-3-opus-20240229"; do
--- run_extract_answer.sh
+++ run_extract_answer.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 # "input_date" "input_date_today" "input_emphasize_url_wiki_wordpress_url"  "input_emphasize_url_cnn_naturalnews_url";
-#  for model in "claude-3-sonnet-20240229"; do
-#      for prompt_template in  "input_emphasize_url_wiki_wordpress_url" "input_emphasize_src_wiki_wordpress_src"; do
-#          for favored_stance in "yes" "no"; do
-#              python3 extract_answer_from_generation.py \
-#                  --answer_file results_fake/generate/"$model"/"$prompt_template"_"$favored_stance".json
-#          done
-#      done
-#  done
+for model in "Llama-2-7b-chat-hf"; do
+    for prompt_template in "input_emphasize_url_wiki_wordpress_url" "input_emphasize_src_wiki_wordpress_src"; do
+        for favored_stance in "yes" "no"; do
+            python3 extract_answer_from_generation.py \
+                --answer_file results/generate/"$model"/"$prompt_template"_"$favored_stance".json
+        done
+    done
+done
 
 # "Meta-Llama-3-8B-Instruct" "Meta-Llama-3-70B-Instruct" "tulu-2-dpo-7b" "Llama-2-7b-chat-hf" "Llama-2-13b-chat-hf"
 
@@ -20,13 +20,13 @@
 #      done
 #  done
 
-for model in "gpt-4o" "claude-3-opus-20240229"; do
-    for prompt_template in "vision_prompts_with_text/yes_simple_no_pretty" "vision_prompts_with_text/yes_pretty_no_simple" "vision_prompts/yes_simple_no_pretty" "vision_prompts/yes_pretty_no_simple"; do
-        python3 extract_answer_from_generation.py \
-            --answer_file results_vision_fake/generate/"$model"/"$prompt_template".json
-
-    done
-done
+# for model in "gpt-4o" "claude-3-opus-20240229"; do
+#     for prompt_template in "vision_prompts_with_text/yes_simple_no_pretty" "vision_prompts_with_text/yes_pretty_no_simple" "vision_prompts/yes_simple_no_pretty" "vision_prompts/yes_pretty_no_simple"; do
+#         python3 extract_answer_from_generation.py \
+#             --answer_file results_vision_fake/generate/"$model"/"$prompt_template".json
+# 
+#     done
+# done
 
 #  for model in "claude-3-haiku-20240307" "claude-3-sonnet-20240229"; do
 #      for prompt_template in "vision_prompts/yes_pretty_no_simple" "vision_prompts/yes_simple_no_pretty"; do
